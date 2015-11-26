0b5adcb100a1efc654d4007aa61e59eb5dbb6d2d
maciek <Maciej.Macowicz@epfl.ch>
2015-11-26 10:56:06 +0100

    Build image from apache directory!!!

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -10,11 +10,8 @@ echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 defaultVariant='apache'
 
 for variant in apache fpm; do
-	commit="$(cd "$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
+	commit="$(git log -1 --format='format:%H' -- "$variant")"
 	fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' "$variant/Dockerfile" | cut -d' ' -f3)"
-	if [[ "$fullVersion" != *.*.* && "$fullVersion" == *.* ]]; then
-		fullVersion+='.0'
-	fi
 
 	versionAliases=()
 	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
@@ -34,3 +31,4 @@ for variant in apache fpm; do
 		echo "$va: ${url}@${commit} $variant"
 	done
 done
+
